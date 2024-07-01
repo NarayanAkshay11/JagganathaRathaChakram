a1bd1089f3dafef327bf91e39e966a511e9eb231
Akshay Narayanan B <90465549+NarayanAkshay11@users.noreply.github.com>
2024-07-01 19:42:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // script.js
 $(document).ready(function() {
-    const sheetId = 'YOUR_GOOGLE_SHEET_ID';
+    const sheetId = '1eCh6XM0azDp5XvM2DPLIjlnnwMQu3WuGOk26H5tkAng';
     const sheetName = 'Sheet1';
-    const apiKey = 'YOUR_GOOGLE_SHEETS_API_KEY';
+    const apiKey = 'AIzaSyB1dC_3xQF7lBU9AyQXoZt3IBw2IUsH8Es';
     const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}?key=${apiKey}`;
 
     let currentIndex = 0;

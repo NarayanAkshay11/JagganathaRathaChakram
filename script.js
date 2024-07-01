340942d08c417af638ab402ad261c41ff7bd12b5
Akshay Narayanan B <90465549+NarayanAkshay11@users.noreply.github.com>
2024-07-01 22:15:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,30 +38,4 @@ function handleTouchStart(evt) {
     const firstTouch = getTouches(evt)[0];                                      
     xDown = firstTouch.clientX;                                      
     yDown = firstTouch.clientY;                                      
-};                                                
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    let xUp = evt.touches[0].clientX;                                    
-    let yUp = evt.touches[0].clientY;
-
-    let xDiff = xDown - xUp;
-    let yDiff = yDown - yUp;
-                                                    
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/* most significant*/
-        if ( xDiff > 0 ) {
-            /* left swipe */ 
-            currentIndex = (currentIndex + 1) % characters.length;
-            updateCarousel();
-        } else {
-            /* right swipe */
-            currentIndex = (currentIndex - 1 + characters.length) % characters.length;
-            updateCarousel();
-        }                       
-    } 
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
 };
